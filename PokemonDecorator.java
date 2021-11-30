1791f83ce2914cd791c51ddc256d855694d4cb0b
Ralfbrse <raphaeloburce@gmail.com>
2021-11-30 13:04:14 -0800

    e

--- PokemonDecorator.java
+++ PokemonDecorator.java
@@ -1,30 +1,58 @@
 public abstract class PokemonDecorator extends Pokemon {
-    private Pokemon pokemon;
+  private Pokemon pokemon;
 
-    
+  public PokemonDecorator(Pokemon p, String extraName, int extraHp) {
+    super(p.getName() + extraName, p.getHp() + extraHp, p.getMaxHp() + extraHp);
+    pokemon = p;
+  }
 
-    public PokemonDecorator(Pokemon p, String extraName, int extraHp){
-
-        super(p.getName() + extraName, p.getHp()+extraHp, p.getMaxHp()+extraHp );
-        this.pokemon = p;
-        
+  @Override
+  public String getAttackMenu(int atkType) {
+    if(atkType == 1) {
+      return super.getAttackMenu(atkType);
     }
-    
+    return pokemon.getAttackMenu(atkType);
+  }
 
-    public String getAttackMenu(int atkType) {
-        return "\n1. Slam \n2. Tackle \n3. Punch";
-        }
-
-    public String getNumAttackMenuItems(){
-        return null;
-        
+  @Override
+  public int getNumAttackMenuItems(int atkType) {
+    if(atkType == 1) {
+      return super.getNumAttackMenuItems(atkType);
     }
+    return pokemon.getNumAttackMenuItems(atkType);
+  }
 
-
-    public String getAttackString(int atkType, int move) {
-        return null;
-        
+  @Override
+  public String getAttackString(int atkType, int move) {
+    if(atkType == 1) {
+      return super.getAttackString(atkType, move);
     }
+    return pokemon.getAttackString(atkType, move);
+  }
 
-    
+  @Override
+  public int getAttackDamage(int atkType, int move) {
+    if(atkType == 1) {
+      return super.getAttackDamage(atkType, move);
+    }
+    return pokemon.getAttackDamage(atkType, move);
+  }
+
+  @Override
+  public double getAttackMultiplier(Pokemon p, int atkType) {
+    if(atkType == 1) {
+      return super.getAttackMultiplier(p, atkType);
+    }
+    return pokemon.getAttackMultiplier(p, atkType);
+  }
+
+  @Override
+  public int getAttackBonus(int atkType) {
+    return super.getAttackBonus(atkType);
+  }
+
+  @Override
+  public int getType() {
+    return super.getType();
+  }
 }
